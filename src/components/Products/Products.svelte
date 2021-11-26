a6215891d7975f0e921130370b49f9edfb3d2aaf
Sergey <mushroomfunk@gmail.com>
2021-11-27 04:05:10 +0500

    back to 60

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { action_destroyer } from 'svelte/internal';
+
 	import Product from './Product.svelte';
 
 	export let products;
@@ -6,7 +8,7 @@
 
 <div class="flex flex-wrap">
 	{#each Array(50) as _, i}
-		{#each products as product}
+		{#each products as product (`${i}—${product.id}`)}
 			<Product {product} />
 		{/each}
 	{/each}
